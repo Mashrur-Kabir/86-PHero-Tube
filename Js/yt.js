ce8dd713035c883f1053130824d11cfa9c49b94b
Mashur-Kabir <mashrurkabirriyan@gmail.com>
2024-08-22 21:48:27 +0600

    more adjustments

--- Js/yt.js
+++ Js/yt.js
@@ -1,3 +1,4 @@
+// Appending buttons from API
 const buttonAction = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`)
     const buttonAllInfo = await res.json();
@@ -14,68 +15,77 @@ const buttonAction = async () => {
 
 buttonAction();
 
-/* const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms)); // delaying the fetching of data to show the loader a bit more */
 
 // loading Videos from API
 const loadVideo = async (categoryId) => {
-    // initiating loader
+    //initiating loader
     toggleLoader(true);
 
-    /* // Delay before starting the fetch operation
-    await delay(900); // Adjust the delay time as needed (e.g., 300ms) */
+    await new Promise(resolve => setTimeout(resolve, 200)); // Add a delay to ensure loader visibility
 
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const dataWhole = await res.json();
     const data = dataWhole.data;
     displayVideos(data);
-}
+};
 
 const displayVideos = (data) => {
 
     const cardSection = document.getElementById('card-section');
     cardSection.textContent = ''; // Clear existing content before adding new ones
 
-    data.forEach(data => {
-        const card = document.createElement('div');
-        card.classList = `rounded-lg overflow-hidden shadow-lg mb-14`;
-
-        // Convert the posted time using the timeConvert function
-        const postedTime = timeConvert(data.others.posted_date);
-
-        // Determine if the author is verified
-        let verifiedIcon = ''; // Initialize the variable
-
-        if (data.authors[0].verified === true) {
-            verifiedIcon = '<img src="icons/social-media.png" alt="" class="">';
-        }
-
-        card.innerHTML = `
-        <div class="relative">
-            <img class="w-full h-[16rem] object-cover" src="${data.thumbnail}" alt="Thumbnail">
-            <!-- Time badge -->
-            <div class="absolute bottom-2 right-2 bg-black bg-opacity-75 text-white text-xs px-2 py-1 rounded" hidden>
-                ${postedTime}
-            </div>
-        </div>
-            <!-- Content Section -->
-        <div class="p-4">
-        <div class="flex items-center mt-3 gap-3">
-            <img class="w-10 h-10 rounded-full" src="${data.authors[0].profile_picture}" alt="User">
-            <h2 class="text-lg font-bold text-gray-900">${data.title}</h2>
-        </div>
-            <div class="mt-1">
-                <div class="ml-[3.22rem]">
-                    <div class="flex items-center mb-1">
-                        <p class="text-sm font-medium text-gray-700">${data.authors[0].profile_name}</p>
-                        <span class="ml-1.5">${verifiedIcon}</span>
-                    </div>
-                    <p class="text-sm text-gray-600">${data.others.views}</p>
+    if (data.length > 0){
+        cardSection.classList.add('grid');
+        data.forEach(data => {
+            const card = document.createElement('div');
+            card.classList = `rounded-lg overflow-hidden shadow-lg mb-14`;
+    
+            // Convert the posted time using the timeConvert function
+            const postedTime = timeConvert(data.others.posted_date);
+    
+            // Determine if the author is verified
+            let verifiedIcon = ''; // Initialize the variable
+    
+            if (data.authors[0].verified === true) {
+                verifiedIcon = '<img src="icons/social-media.png" alt="" class="">';
+            }
+    
+            card.innerHTML = `
+            <div class="relative">
+                <img class="w-full h-[16rem] object-cover" src="${data.thumbnail}" alt="Thumbnail">
+                <!-- Time badge -->
+                <div class="absolute bottom-2 right-2 bg-black bg-opacity-75 text-white text-xs px-2 py-1 rounded" hidden>
+                    ${postedTime}
                 </div>
             </div>
+                <!-- Content Section -->
+            <div class="p-4">
+            <div class="flex items-center mt-3 gap-3">
+                <img class="w-10 h-10 rounded-full" src="${data.authors[0].profile_picture}" alt="User">
+                <h2 class="text-lg font-bold text-gray-900">${data.title}</h2>
+            </div>
+                <div class="mt-1">
+                    <div class="ml-[3.22rem]">
+                        <div class="flex items-center mb-1">
+                            <p class="text-sm font-medium text-gray-700">${data.authors[0].profile_name}</p>
+                            <span class="ml-1.5">${verifiedIcon}</span>
+                        </div>
+                        <p class="text-sm text-gray-600"><span id="viewNumber">${data.others.views}</span></p>
+                    </div>
+                </div>
+            </div>
+            `
+            cardSection.appendChild(card);
+        });
+    }else{
+        cardSection.classList.remove('grid');
+        cardSection.innerHTML = `
+        <div class="flex flex-col items-center justify-center my-40">
+            <img class="w-1/6 mb-10" src="icons/noResFound.png" alt=":(">
+            <h1 class="text-5xl font-bold leading-tight">Oops!!Sorry, There is no Content Here</h1>
         </div>
         `
-        cardSection.appendChild(card);
-    });
+    }
     toggleLoader(false); // hide spinning loader after successful data load
 };
 
@@ -106,3 +116,4 @@ const toggleLoader = (isLoading) => {
         spinLoader.classList.add('hidden');
     }
 }
+
